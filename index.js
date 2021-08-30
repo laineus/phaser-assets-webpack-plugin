02bc4d8b6f104761a547a2b6d6200664a8124d2d
laineus <lainen000@gmail.com>
2021-08-30 21:07:50 +0900

    make lib

--- index.js
+++ index.js
@@ -1,90 +1,22 @@
-const path = require('path')
-const fs = require('fs')
-const sizeOf = require('image-size')
-
-const defaultSettings = {
-  documentRoot: 'public',
-  output: 'assets.json',
-  spriteSheetSettingsFileName: 'settings.json'
-}
+const useAssetsLoader = require('./lib/useAssetsLoader.js')
 
 module.exports = class {
   constructor (patterns, settings) {
     this.patterns = patterns
-    this.settings = { ...defaultSettings, ...settings }
-    this.timer = null
+    this.settings = settings
   }
   apply (compiler) {
     this.projectRoot = compiler.context
+    const { exportJson, watch } = useAssetsLoader(this.patterns, this.settings, { projectRoot: compiler.context })
     compiler.hooks.afterEnvironment.tap('AssetsPlugin', () => {
-      console.log('PhaserAssetsWebpackPlugin: Initializing...')
-      this.exportJson()
-      if (compiler.options.mode === 'development') this.watch()
+      exportJson()
+      console.log('PhaserAssetsWebpackPlugin: Initialized')
+      if (compiler.options.mode === 'development') {
+        console.log('PhaserAssetsWebpackPlugin: Watching...')
+        watch(() => {
+          console.log('PhaserAssetsWebpackPlugin: Reloaded')
+        })
+      }
     })
-  }
-  watch () {
-    // Watch dependent directories
-    this.patterns.map(v => path.join(this.projectRoot, this.settings.documentRoot, v.dir)).forEach(dir => {
-      fs.watch(dir, _event => {
-        clearTimeout(this.timer)
-        this.timer = setTimeout(() => {
-          console.log('PhaserAssetsWebpackPlugin: Reloading...')
-          this.exportJson()
-        }, 2000)
-      })
-    })
-  }
-  exportJson () {
-    const assetsData = this.getAssetsData()
-    const json = JSON.stringify(assetsData, null, 2)
-    const outputPath = path.join(this.projectRoot, this.settings.output)
-    fs.writeFileSync(outputPath, json)
-  }
-  getAssetsData () {
-    const data = this.patterns.reduce((result, pattern) => {
-      const dir = path.join(this.projectRoot, this.settings.documentRoot, pattern.dir)
-      const fileNames = fs.readdirSync(dir)
-      const spriteSheetSettings = this.getSpriteSheetSettings(dir)
-      const list = fileNames.filter(fileName => pattern.rule.test(fileName)).reduce((list, fileName) => {
-        const assetKeyName = `${pattern.prefix}${fileName.split('.')[0]}`
-        const url = `${pattern.dir}${pattern.dir.endsWith('/') ? '' : '/'}${fileName}`
-        const sameKeyRow = list.find(v => v[0] === assetKeyName)
-        if (sameKeyRow) {
-          // Append the file if existing same key
-          sameKeyRow.splice(1, 1, [sameKeyRow[1], url].flat())
-        } else {
-          const spriteSheetSetting = spriteSheetSettings && spriteSheetSettings.find(v => v[0] === fileName)
-          const spriteSheetOption = spriteSheetSetting && this.getSpriteSheetOption(path.join(dir, fileName), spriteSheetSetting[1], spriteSheetSetting[2])
-          list.push(spriteSheetOption ? [assetKeyName, url, spriteSheetOption] : [assetKeyName, url])
-        }
-        return list
-      }, [])
-      if (pattern.callback) pattern.callback(list)
-      result[pattern.type] = result[pattern.type] ? result[pattern.type].concat(list) : [...list]
-      return result
-    }, {})
-    data.spritesheet = data.image.filter(v => v.length === 3)
-    data.image = data.image.filter(v => v.length === 2)
-    if (!data.spritesheet.length) delete data.spritesheet
-    if (!data.image.length) delete data.image
-    return data
-  }
-  getSpriteSheetSettings (dir) {
-    const pathToSettings = path.join(dir, this.settings.spriteSheetSettingsFileName)
-    if (!fs.existsSync(pathToSettings)) return null
-    const settingsJson = fs.readFileSync(pathToSettings, 'utf8')
-    try {
-      return JSON.parse(settingsJson)
-    } catch (error) {
-      console.error(`[Error] Invalid JSON String: ${pathToSettings}`)
-      return null
-    }
-  }
-  getSpriteSheetOption (filePath, numOfX, numOfY) {
-    const { width, height } = sizeOf(filePath)
-    const frameWidth = Math.round(width / numOfX)
-    const frameHeight = Math.round(height / numOfY)
-    const endFrame = (numOfX * numOfY) - 1
-    return { frameWidth, frameHeight, startFrame: 0, endFrame }
   }
 }
